857fc4a7a108d7436f548bf6e604d04e79b3a2ae
Michael Pope <mpope.cr@gmail.com>
2015-06-12 16:20:16 -0400

    Add debug example with local variables

--- lib/debugger.rb
+++ lib/debugger.rb
@@ -1,25 +1,34 @@
-# How does IRB work?
-def repl(binding)
-  loop do
-    print "irb knockoff> "    # input prompt
-    input = gets.chomp        # grab the user input
-    break if input == "break" # if needed, breaks out of the loop
-
-    begin
-      result = eval(input, binding)         # evaluates the input
-      puts  "=> #{result.inspect}"          # print the result
-    rescue Exception => e                   # catches an exception
-      puts e.message                        # prints error message
-      e.backtrace.each { |l| puts "  #{l}"} # prints backtrace
-    end
-  end
-end
+require_relative "repl"
 
 # How does Pry give you an IRB like prompt in a running program?
 def debug(binding)
-  repl(binding)
+  should_start_repl = false
+
+  trace_point = TracePoint.new do |tp|
+    $tp = tp # try avoiding global variables in general!
+
+    if should_start_repl
+      repl(binding)
+    else
+      if tp.event == :return && tp.method_id == :debug
+        should_start_repl = true
+      end
+    end
+  end
+
+  trace_point.enable
 end
 
-a = 123
-debug(binding)
-b = 456
+# How does Pry give you access to local variables?
+def program(binding_1)
+  a = 123
+  b = 456
+  binding_2 = binding
+  puts "debugging with binding_1 (a & b not yet local variables)"
+  debug binding_1
+  puts "debugging with binding_2 (a equals 123, b equals 456)"
+  debug binding_2
+end
+
+binding_1 = binding
+program binding_1
